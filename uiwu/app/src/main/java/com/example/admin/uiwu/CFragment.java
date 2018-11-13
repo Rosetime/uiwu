b3e3d48e33f904f80d72b4742864e2fc1e8f715d
Rosetime <37096345+Rosetime@users.noreply.github.com>
2018-11-13 21:57:41 +0800

    Update uiwu/app/src/main/java/com/example/admin/uiwu/CFragment.java

---
+++ uiwu/app/src/main/java/com/example/admin/uiwu/CFragment.java
@@ -0,0 +1,28 @@
+package com.example.admin.uiwu;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_b,container,false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+}
